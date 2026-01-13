951de72cc9bb0c6c24c1f42260ac985ba5617459
Mueem <rusafkakhilji@gmail.com>
2026-01-13 12:16:32 +0500

    Update helper_option_templates_for_arithmetic_related_options.cpp
    I keep on doing this one mistake that for some reason I am too stupid to correct, like my neck hurts, so I am just tyring to ask AI for errors instead of like using the compiler

--- Output_modules/src/helper_option_templates_for_arithmetic_related_options.cpp
+++ Output_modules/src/helper_option_templates_for_arithmetic_related_options.cpp
@@ -43,10 +43,11 @@ namespace printing_tools{
             }
             T convert_to_number(std::string source) {
                 std::string::size_type position = 0;
+		T result= read_number_from_string_at_a_position<T>(source, &position);
  		if (position != source.length()-1) {
                     throw std::runtime_error{ "number mixed with (non numeric) charactor while taking input for an option" };
                 }
-                return read_number_from_string_at_a_position<T>(source, &position);
+                return result;
                
             }
                template <absolute_base::Is_String_Or_Numeric T_dest, absolute_base::Is_String_Or_Numeric T_source> 
