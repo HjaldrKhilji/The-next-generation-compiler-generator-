19f365050a5a86e18928b488e2e3438cde99516f
Mueem <rusafkakhilji@gmail.com>
2026-01-27 13:53:26 +0500

    Update and rename Test_code.cpp to run.cpp

---
+++ run.cpp
@@ -0,0 +1,45 @@
+#include <iostream>  // For std::cerr and std::endl
+#include <string>    // For argv processing (if needed by your engine)
+// I used AI to keep track of the headers needed
+
+import Inputer;
+import Printer;
+import Driver
+import All_declarations;
+
+
+int main(int argc, char* argv[]) {
+	using index = driver::index_from_the_end_of_each_argument_of_single_engine;
+	if(argc!=9){
+	std::cerr<<"Minimum of 10 arguments required to start"<<std::endl;
+	}
+	else if (!(argc % 9)) {
+		std::ostream{ argv[argc - index::error_log_file_for_config] } << "each engine instance needs 9 arguments";
+		std::cerr << "each engine instance needs 9 arguments" << std::endl;
+	}
+	else {
+		int currently_to_be_processed_size = 9;
+
+		do {
+			
+			driver::Driver_engine engine = driver::create_driver(
+				argv[currently_to_be_processed_size - index::debug_mode],
+				argv[currently_to_be_processed_size - index::debug_mode_for_reading_output_config],
+				argv[currently_to_be_processed_size - index::debug_mode_for_reading_input_config],
+				argv[currently_to_be_processed_size - index::error_log_file],
+				argv[currently_to_be_processed_size - index::error_log_file_for_config],
+				argv[currently_to_be_processed_size - index::output_file],
+				absolute_base::convert_to_bool(argv[currently_to_be_processed_size - index::input_file]),
+				absolute_base::convert_to_bool(argv[currently_to_be_processed_size - index::output_config_file]),
+				absolute_base::convert_to_bool(argv[currently_to_be_processed_size - index::input_config_file])
+			);
+			engine.run_engine();
+		
+		} while (current_to_be_processed_size<=argc);
+
+	}
+  
+	
+  return 0;
+}
+
