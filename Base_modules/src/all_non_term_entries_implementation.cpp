5c2d0523f496e81c4c98b1fe1745ac84b69fe427
Mueem <rusafkakhilji@gmail.com>
2026-01-29 21:02:45 +0500

    Update all_non_term_entries_implementation.cpp

--- Base_modules/src/all_non_term_entries_implementation.cpp
+++ Base_modules/src/all_non_term_entries_implementation.cpp
@@ -18,25 +18,28 @@ module;
 
 module all_declarations;
 import estd_regex;
-using  absolute_base::All_non_terminal_entries_implementation::Iterator_for_list_of_entries; 
 using absolute_base::Settings_for_semantical_rules;
+template<typename config>
 using absolute_base::All_non_terminal_entries_implementation;
 
+template<typename config>
 void All_non_terminal_entries_implementation::add_a_child_to_entry
-(absolute_base::Non_terminal_name_entry *entry_to_be_added_to,  absolute_base::Non_terminal_name_entry&& entry_to_add) override{
+(absolute_base::Non_terminal_name_entry<config> *entry_to_be_added_to,  absolute_base::Non_terminal_name_entry<config>&& entry_to_add) override{
     if (!map_for_fast_retrival_of_entries.contains(entry_to_be_added_to->name)) {
         throw std::string{ "COMPILER: entry not found" };
     }
     map_for_fast_retrival_of_entries[entry_to_be_added_to->name]->sub_entry.push_back(std::move(entry_to_add));
 }
-void All_non_terminal_entries_implementation::add_semantic_rule_to_entry(absolute_base::Non_terminal_name_entry* entry_to_be_added_to,  Semantical_analyzer_config_entry&& semantical_rule_entry, int sibling_index, int semantic_entry_index) override {
+template<typename config>
+void All_non_terminal_entries_implementation::add_semantic_rule_to_entry(absolute_base::Non_terminal_name_entry<config>* entry_to_be_added_to,  Semantical_analyzer_config_entry<config>&& semantical_rule_entry, int sibling_index, int semantic_entry_index) override {
     auto& semantic_rules_for_entry = entry_to_be_added_to.all_semantical_analysis_rules[sibling_index];
     semantic_rules_for_entry.insert(semantic_rules_for_entry.begin() + semantic_entry_index, std::move(semantical_rule_entry));
 
 }
+template<typename config>
 void All_non_terminal_entries_implementation::remove_entry
-(absolute_base::Non_terminal_name_entry* entry_to_remove) override {
-    Iterator_for_list_of_entries map_iterator = map_for_fast_retrival_of_entries.find(entry_to_remove->name);
+(absolute_base::Non_terminal_name_entry<config>* entry_to_remove) override {
+    auto map_iterator = map_for_fast_retrival_of_entries.find(entry_to_remove->name);
     //it is the caller's(the whole projects) job to make sure that entry_to_remove is in the map
     
 
@@ -46,23 +49,25 @@ void All_non_terminal_entries_implementation::remove_entry
 
     
 }
+template<typename config>
 
-void All_non_terminal_entries_implementation::remove_semantic_rule_of_entry(absolute_base::Non_terminal_name_entry* entry_to_remove_from, int sibling_index, int index_of_semantic_rule) override {
+void All_non_terminal_entries_implementation::remove_semantic_rule_of_entry(absolute_base::Non_terminal_name_entry<config>* entry_to_remove_from, int sibling_index, int index_of_semantic_rule) override {
     auto& semantic_rules_for_entry = entry_to_remove_from.all_semantical_analysis_rules[sibling_index];
     semantic_rules_for_entry.erase(v.begin() + index_of_semantic_rule);
 }
 
+template<typename config>
 
-void absolute_base::Semantical_analyzer_config_entry::check_pattern(const std::vector < Semantical_analyzer_config_entry >& semantical_checks, std::string text) {
+void absolute_base::Semantical_analyzer_config_entry::check_pattern(const std::vector < Semantical_analyzer_config_entry<config> >& semantical_checks, const std::string& text) {
     std::string failed_matches{};
+    uint64_t semantic_entry_index{};
     for (auto& x : semantical_checks) {
-        estd::regex pattern{ x.the_pattern_to_check };
-
-        int number_of_matches = static_cast<size_t>(std::distance(
-            estd::sregex_iterator(text.begin(), text.end(), pattern),
-            estd::sregex_iterator() // Default constructor represents the end-of-sequence, like the first argument will be equal to the seocnd, when the first argument reaches the end
-        ));
-        if (all_settings == Settings_for_semantical_rules::NONE) {
+        uint64_t number_of_matches{};
+        while(x.regex_match(text)){
+            number_of_matches++;
+        }
+        if (all_settings == Settings_for_semantical_rules::NONE && number_of_matches) {
+           failed_matches += "failed match index" + std::to_string(semantic_entry_index) + '\n';
 
         }
         else if (all_settings && Settings_for_semantical_rules::check_exist) {
@@ -71,12 +76,12 @@ void absolute_base::Semantical_analyzer_config_entry::check_pattern(const std::v
                     //means the useer entered -1 as maximum_amount_of_matches
                      //which means that he dosent want us to check the upper limit at all
                     if (number_of_matches > minimum_amount_of_matches) {
-                        failed_matches += x.the_pattern_to_check + '\\' + '\n';
+                       failed_matches += "failed match index" + std::to_string(semantic_entry_index) + '\n';
                     }
                 }
                 else {
                     if ((maximum_amount_of_matches < number_of_matches) && (number_of_matches > minimum_amount_of_matches)) {
-                        failed_matches += x.the_pattern_to_check + '\\' + '\n';
+                        failed_matches += "failed match index" + std::to_string(semantic_entry_index) + '\n';
                     }
                 }
             }
@@ -84,7 +89,7 @@ void absolute_base::Semantical_analyzer_config_entry::check_pattern(const std::v
             {
 
                 if (number_of_matches != minimum_amount_of_matches) {
-                    failed_matches += x.the_pattern_to_check + '\\' + '\n';
+                   failed_matches += "failed match index" + std::to_string(semantic_entry_index) + '\n';
                 }
 
             }
@@ -92,21 +97,22 @@ void absolute_base::Semantical_analyzer_config_entry::check_pattern(const std::v
 
         else if (all_settings && Settings_for_semantical_rules::check_dont_exist) {
             if (!number_of_matches) {
-                failed_matches += x.the_pattern_to_check + '\\' + '\n';
+                failed_matches += "failed match index" + std::to_string(semantic_entry_index) + '\n';
             }
         }
+        semantic_entry_index++;
     }
     if (failed_matches.length() != 0) {
         throw std::string{std::move(failed_matches)};
     }
     }
 
-
+template<typename config>
 void All_non_terminal_entries_implementation::print_all_content() {
     //precondition:*this's  invariant is valid
     
     std::cout << "number of entries: " << list_of_all_non_term_entries_for_fast_traversal.size() << std::endl;
-    for (const absolute_base::Non_terminal_name_entry &current_entry : list_of_all_non_term_entries_for_fast_traversal) {
+    for (const auto &current_entry : list_of_all_non_term_entries_for_fast_traversal) {
         std::cout << "current entry:" << std::endl;
         std::cout << current_entry.name << " ";
         std::cout << current_entry.pattern << " ";
@@ -131,7 +137,7 @@ void All_non_terminal_entries_implementation::print_all_content() {
         std::cout << "next entry(if any):" << std::endl;
     }
 }
-
+template<typename config>
 void All_non_terminal_entries_implementation::add_non_term_symbol_name(uint64_tname) {
   //precondition: "name" paremeter can not be empty
   //precondition: list_of_all_non_term_entries_for_fast_traversal is in a valid state 
@@ -140,20 +146,21 @@ void All_non_terminal_entries_implementation::add_non_term_symbol_name(uint64_tn
 
     list_of_all_non_term_entries_for_fast_traversal.push_back(Non_terminal_name_entry {
       name,
-      "",
+      config{},
      Non_terminal_name_entry::sub_entry_type{},
       Non_terminal_name_entry::all_semantical_analysis_rules_type{}
     });
 
   }
-  void All_non_terminal_entries_implementation::add_non_term_pattern_for_newest_entry(std::string pattern) {
+template<typename config>
+  void All_non_terminal_entries_implementation::add_non_term_pattern_for_newest_entry(config pattern) {
     //precondition: "pattern" paremeter dose'nt contain a invalid regex expression
     //precondition: list_of_all_non_term_entries_for_fast_traversal is in a valid state and contains atleast a single entry
     //precondition: map_for_fast_retrival_of_entries is also in a valid state, and is amenable(mutable) enough to have a data inserted into it
     //postcondition: list_of_all_non_term_entries_for_fast_traversal gets the pattern for its latest entry entered
     //postcondition: map_for_fast_retrival_of_entries gets the "name" data member of the latest entry as key and the latest entry itself
     //entered as a entry into map_for_fast_retrival_of_entries
-    Iterator_for_list_of_entries& newest_entry = list_of_all_non_term_entries_for_fast_traversal.end();
+    auto newest_entry = list_of_all_non_term_entries_for_fast_traversal.end();
     newest_entry->pattern = pattern;
     //notice that this also a adds the entry to the map for fast retrival:
      map_for_fast_retrival_of_entries.insert({
@@ -162,7 +169,8 @@ void All_non_terminal_entries_implementation::add_non_term_symbol_name(uint64_tn
     });
 
   }
-  std::string & All_non_terminal_entries_implementation::get_pattern_of_nested_non_term_symbol_pattern(uint64_t sub_symbol_name) {
+template<typename config>
+  config& All_non_terminal_entries_implementation::get_pattern_of_nested_non_term_symbol_pattern(uint64_t sub_symbol_name) {
   //precondition: "sub_symbol_name" paremeter can not be empty
   //precondition: map_for_fast_retrival_of_entries is in a valid state and is not empty
   //postcondition: the entry corrsponding to the key "sub_symbol_name" from map_for_fast_retrival_of_entries, and the copy of the 
@@ -175,6 +183,7 @@ void All_non_terminal_entries_implementation::add_non_term_symbol_name(uint64_tn
     return (map_for_fast_retrival_of_entries.at(sub_symbol_name))->pattern;
 
   }
+template<typename config>
   std::reference_wrapper < std::string > All_non_terminal_entries_implementation::get_parmenant_name_of_nested_non_term_symbol_pattern(uint64_t sub_symbol_name) {
 //precondition: "sub_symbol_name" paremeter can not be empty
   //precondition: map_for_fast_retrival_of_entries is in a valid state and is not empty
@@ -189,6 +198,7 @@ void All_non_terminal_entries_implementation::add_non_term_symbol_name(uint64_tn
     return map_for_fast_retrival_of_entries.at(sub_symbol_name)->name;
 
   }
+template<typename config>
   void All_non_terminal_entries_implementation::add_nested_non_term_symbol_to_the_newest_entry(uint64_t sub_symbol_name) {
     //precondition: "sub_symbol_name" paremeter can not be empty
     //precondition: map_for_fast_retrival_of_entries is in a valid state and is not empty
@@ -196,14 +206,14 @@ void All_non_terminal_entries_implementation::add_non_term_symbol_name(uint64_tn
     //postcondition: the "sub_entries" field has a reference wrapper that points to a nested field appended to itself
 
 
-      Non_terminal_name_entry newest_entry& = list_of_all_non_term_entries_for_fast_traversal.back();
-      Iterator_for_list_of_entries sub_entry_that_corrosponds_to_sub_name = map_for_fast_retrival_of_entries.at(sub_symbol_name);
+      auto newest_entry = list_of_all_non_term_entries_for_fast_traversal.back();
+      auto sub_entry_that_corrosponds_to_sub_name = map_for_fast_retrival_of_entries.at(sub_symbol_name);
 
     newest_entry.sub_entries.push_back(*sub_entry_that_corrosponds_to_sub_name);
 
   }
-
-  void All_non_terminal_entries_implementation::add_semantic_rule_for_newest_sub_entry(const absolute_base::Semantical_analyzer_config_entry && semantical_rule_entry) {
+template<typename config>
+  void All_non_terminal_entries_implementation::add_semantic_rule_for_newest_sub_entry<config>(const absolute_base::Semantical_analyzer_config_entry<config> && semantical_rule_entry) {
     //I am using const rvalue reference to force the programmer to first gather all information, and then built the object using that in the same line as calling the function
     //hence avoiding any idiotic errors that he could do with such a important data structure, while trying to be "clever"
     //precondition: the paremeter "semantical_rule_entry" is in a valid state
@@ -212,10 +222,10 @@ void All_non_terminal_entries_implementation::add_non_term_symbol_name(uint64_tn
     //postcondition: all_semantical_analysis_rules has a new semantic rule pushed to the newest element of itself
     //every element in "all_semantical_analysis_rules" has a semantic rule for the corrsponding element in "sub_entries" data member, and corrsponding 
     // in this specific sentence means the element with the same index.
-    auto & newest_entry = list_of_all_non_term_entries_for_fast_traversal.back();;
+    auto  newest_entry = list_of_all_non_term_entries_for_fast_traversal.back();;
     if (newest_entry.sub_entries.size() -1 == newest_entry.all_semantical_analysis_rules.size()) {
       //this means thats it the first semantic entry for the nested name
-      newest_entry.all_semantical_analysis_rules.push_back(std::vector < Semantical_analyzer_config_entry > {
+      newest_entry.all_semantical_analysis_rules.push_back(std::vector < Semantical_analyzer_config_entry<config> > {
         std::move(semantical_rule_entry)
       });
       return;
@@ -228,13 +238,15 @@ void All_non_terminal_entries_implementation::add_non_term_symbol_name(uint64_tn
       throw std::string("COMPILER: add_semantic_rule_for_newest_sub_entry error");
     }
   }
-  absolute_base::Non_terminal_name_entry& All_non_terminal_entries_implementation::get_current_non_term_entry(int index) {
+template<typename config>
+  absolute_base::Non_terminal_name_entry<config>& All_non_terminal_entries_implementation::get_current_non_term_entry(int index) {
       //precondition:index is not negative and list_of_all_non_term_entries_for_fast_traversal is in a valid state
       //postcondition: returns a non term entry at the given index to the caller
       return list_of_all_non_term_entries_for_fast_traversal[index];
 
   }
-  absolute_base::Non_terminal_name_entry& All_non_terminal_entries_implementation::get_current_nested_non_term_entry(int index_for_nested_non_term, int index) {
+template<typename config>
+  absolute_base::Non_terminal_name_entry<config>& All_non_terminal_entries_implementation::get_current_nested_non_term_entry(int index_for_nested_non_term, int index) {
       //precondition:the given indexes are not negative and list_of_all_non_term_entries_for_fast_traversal is in a valid state
     //postcondition: returns a nested non term entry at the index "[index][index_for_nested_non_term]"
       return *(list_of_all_non_term_entries_for_fast_traversal[index].sub_entries[index_for_nested_non_term]);
