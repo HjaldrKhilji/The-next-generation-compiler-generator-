b711380389b86917b31d380baf5e686d97054121
Mueem <rusafkakhilji@gmail.com>
2026-02-06 18:14:45 +0500

    Update run.cpp

--- run.cpp
+++ run.cpp
@@ -10,15 +10,17 @@ import All_declarations;
 
 int main(int argc, char* argv[]) {
 	using index = driver::index_from_the_end_of_each_argument_of_single_engine;
-	if(argc!=11){
-	std::cerr<<"Minimum of 11 arguments required to start"<<std::endl;
+	if(argc!=14){
+	std::cerr<<"Minimum of 14 arguments required to start"<<std::endl;
 	}
-	else if (!(argc % 11)) {
-		std::ostream{ argv[argc - index::error_log_file_for_config] } << "each engine instance needs 9 arguments";
-		std::cerr << "each engine instance needs 11 arguments" << std::endl;
+	else if (!(argc % 14)) {
+		if(argc % 14==index::error_log_file_for_config){
+		std::ostream{ argv[argc - index::error_log_file_for_config] } << "each engine instance needs 14 arguments";
+		}
+		std::cerr << "each engine instance needs 14 arguments" << std::endl;
 	}
 	else {
-		int currently_to_be_processed_size = 11;
+		int currently_to_be_processed_size = 14;
 
 		do {
 			
@@ -33,7 +35,11 @@ int main(int argc, char* argv[]) {
 				absolute_base::convert_to_bool(argv[currently_to_be_processed_size - index::output_config_file]),
 				absolute_base::convert_to_bool(argv[currently_to_be_processed_size - index::input_config_file]),
 				absolute_base::convert_to_bool(argv[currently_to_be_processed_size - index::multi_threaded_launch]),
-				absolute_base::convert_to_bool(argv[currently_to_be_processed_size - index::run_engine_optimized_using_multi_threads])
+				absolute_base::convert_to_bool(argv[currently_to_be_processed_size - index::run_engine_optimized_using_multi_threads]),
+				absolute_base::convert_to_char(argv[currently_to_be_processed_size - index::default_input_config_delimeter]),
+				absolute_base::convert_to_char(argv[currently_to_be_processed_size - index::default_output_config_delimeter]),
+				absolute_base::convert_to_char(argv[currently_to_be_processed_size - index::input_delimeter])
+
 			);
 			engine.run_engine();
 		
@@ -44,4 +50,5 @@ int main(int argc, char* argv[]) {
 	
   return 0;
 }
+
 
